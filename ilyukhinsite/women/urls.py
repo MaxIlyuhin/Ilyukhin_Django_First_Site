bb6b4a211409e06152caf67a89a6ab6c815fd0c4
MaxIlyuhin <iliuhin.maxim2016@yandex.ru>
2023-04-30 16:32:36 +0300

    Templates_The_Beginning

--- ilyukhinsite/women/urls.py
+++ ilyukhinsite/women/urls.py
@@ -4,6 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),    # https://127.0.0.1:8000/
-    path('cats/<int:catid>/', categories),  # https://127.0.0.1:8000/cats/
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive)
+    path('about/', about, name='about')
 ]
