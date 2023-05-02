afc83a7b899c3732674b3b2ea19c58db2e1945a5
MaxIlyuhin <iliuhin.maxim2016@yandex.ru>
2023-05-02 13:21:14 +0300

    Adding Registration

--- ilyukhinsite/women/urls.py
+++ ilyukhinsite/women/urls.py
@@ -3,11 +3,12 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('', WomenHome.as_view(), name='home'),    # https://127.0.0.1:8000/
+    path('', WomenHome.as_view(), name='home'),
     path('about/', about, name='about'),
     path('addpage/', AddPage.as_view(), name='add_page'),
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
+    path('register/', RegisterUser.as_view(), name='register'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
-    path('category/<slug:cat_slug>/', WomenCategory.as_view(), name="category")
+    path('category/<slug:cat_slug>/', WomenCategory.as_view(), name='category'),
 ]
--- ilyukhinsite/women/views.py
+++ ilyukhinsite/women/views.py
@@ -10,6 +10,7 @@ from .forms import *
 from .models import *
 from .utils import *
 
+
 class WomenHome(DataMixin, ListView):
     model = Women
     template_name = 'women/index.html'
@@ -134,3 +135,12 @@ class WomenCategory(DataMixin, ListView):
 #
 #     return render(request, 'women/index.html', context=context)
 
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'women/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(c_def.items()))
